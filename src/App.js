93a9efc5be8b88cfffba0acdb7fcd03b40c98bba
NguyenVanPhi92 <83050558+NguyenVanPhi92@users.noreply.github.com>
2022-10-18 11:18:07 +0700

    project initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import AccessibleForwardIcon from '@material-ui/icons/AccessibleForward'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            hi
+            <AccessibleForwardIcon className="fa fa-plus-circle" />
+        </div>
+    )
 }
 
-export default App;
+export default App
